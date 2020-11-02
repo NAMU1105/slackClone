0718202bbf96f8ee89e6833de2591c0931aa5d38
NAMU1105 <47317129+NAMU1105@users.noreply.github.com>
2020-11-02 14:07:19 +0900

    user typing, show online user lists funtion added

--- index.js
+++ index.js
@@ -8,21 +8,42 @@ const port = 3002 || process.env.PORT;
 // io는 socket.io 패키지를 import한 변수
 const io = require("socket.io")(http);
 
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require("./utils/users");
+
 const admin = "Admin";
+const welcomMessage = "Welcome";
 
 // app.use(express.static(__dirname));
 // app.use(express.static("/public"));
 app.use(express.static(__dirname + "/public"));
 
+// joined user list
+// const users = [];
+
 // TODO: TODOLIST
-// 1. Broadcast a message to connected users when someone connects or disconnects. -> done
+// 1. Broadcast a message to connected users when someone connects or disconnects.
+// => done
 // 2. Add support for nicknames.
-// 3. Don’t send the same message to the user that sent it. Instead, append the message directly as soon as he/she presses enter.
+// => done
+// 3. Don’t send the same message to the user that sent it.
+// Instead, append the message directly as soon as he/she presses enter.
 // 4. Add “{user} is typing” functionality.
+// => done
 // 5. Show who’s online.
-// 6. Add private messaging.
+// => done
+// 6. Add private messaging. 1대1 말하는건가?
+// -> 유저 클릭하면 방으로 넘어가는 걸로 하기
 // 7. Share your improvements!
+
+////////////////////////////////////////////////
 // 8. 기존에 보냈던 메시지들 보여주기
+// 9. 타이핑 효과 더 스무스하게 보이게 만들기(중요도 하)
+//
 
 app.get("/", (req, res) => {
   // res.send("<h1>Hello world!!!  </h1>");
@@ -41,20 +62,77 @@ io.emit("some event", {
 io.on("connection", (socket) => {
   // socket은 커넥션이 성공했을 때 커넥션에 대한 정보를 담고 있는 변수
 
+  // *********************************************
+  // group chatting
+  // *********************************************
   socket.on("joinRoom", ({ username, room }) => {
     console.log(`username: ${username}`);
     console.log(`room: ${room}`);
-    socket.emit("chat message", "Welcome", admin);
+
+    const userID = socket.id;
 
     socket.join(room);
+
+    // let joinedUser = {
+    //   userID,
+    //   room,
+    //   username,
+    // };
+
+    // users.push(joinedUser);
+    // console.log(`users: ${users}`);
+    userJoin(userID, username, room);
+    let roomUsers = getRoomUsers(room);
+
+    socket.emit("message", welcomMessage, admin, room, roomUsers);
 
     // Broadcast when a user connects
     // socket.broadcast.emit("chat message", "A user entered", admin);
     // socket.broadcast.to(room).emit("chat message", "A user entered", admin);
     socket.broadcast
       .to(room)
-      .emit("chat message", `${username} has joined the chat`, admin);
+      .emit(
+        "message",
+        `${username} has joined the chat`,
+        admin,
+        room,
+        roomUsers
+      );
+
+    socket.on("disconnect", () => {
+      // console.log("user disconnected");
+      userLeave(userID);
+      roomUsers = getRoomUsers(room);
+
+      io.to(room).emit(
+        "message",
+        `${username} has left the chat`,
+        admin,
+        room,
+        roomUsers
+      );
+    });
+
+    // Listen for chatMessage
+    socket.on("chatMessage", (msg) => {
+      // In this case, for the sake of simplicity we’ll send the message to everyone, including the sender.
+      // io.emit("chat message", msg);
+      io.to(room).emit("message", msg, username);
+      console.log("message: " + msg);
+    });
+    socket.on("keyDown", () => {
+      socket.broadcast.to(room).emit("typing", `${username}`);
+    });
+    // TODO: 여러 명이 타이핑 하다가 한명만 빠진 걸 수도 있으니 다시 새로 바뀐 타이핑 중인 리스트 보내주기
+    socket.on("keyUp", () => {
+      socket.broadcast.to(room).emit("doneTyping", `${username}`);
+    });
   });
+
+  // *********************************************
+  // private chatting
+  // *********************************************
+  socket.on("privateChatting", ({ username, room }) => {});
 
   // If you want to send a message to everyone except for a certain emitting socket,
   // we have the broadcast flag for emitting from that socket:
@@ -72,12 +150,13 @@ io.on("connection", (socket) => {
   // console.log(`socket: ${JSON.stringify(socket)}`);
   // console.log(`socket ID: ${socket.id}`);
 
-  // Listen for chatMessage
-  socket.on("chatMessage", (msg) => {
-    // In this case, for the sake of simplicity we’ll send the message to everyone, including the sender.
-    io.emit("chat message", msg);
-    console.log("message: " + msg);
-  });
+  // // Listen for chatMessage
+  // socket.on("chatMessage", (msg) => {
+  //   // In this case, for the sake of simplicity we’ll send the message to everyone, including the sender.
+  //   // io.emit("chat message", msg);
+  //   io.to(room).emit("chat message", msg);
+  //   console.log("message: " + msg);
+  // });
 });
 
 // socket.on("disconnect", () => {
--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,17 @@
 // TODO:
-// 1. 스크롤 내리기
+// 1. 메시지 보내면 맨 하단으로 스크롤 내리기
 
 const chatForm = document.querySelector("#chatForm");
 const socket = io();
 
 const messages = document.querySelector("#messages");
+const typingSpan = document.querySelector(".typing-info > span");
+const headerRoomName = document.querySelector("header > h1");
+const userList = document.querySelector(".nav__users-list");
+
+const keyDownOne = " is typing";
+const keyDownTwo = " are typing";
+const keyDownSeveral = "Several people are typing";
 
 // Get username and room from URL
 const { username, room } = Qs.parse(location.search, {
@@ -13,6 +20,46 @@ const { username, room } = Qs.parse(location.search, {
 
 // Join chatroom
 socket.emit("joinRoom", { username, room });
+
+// Get room and users
+socket.on("roomUsers", ({ room, users }) => {
+  outputRoomName(room);
+  outputUsers(users);
+});
+
+// // Add users to DOM
+// const outputUsers = (users) => {
+//   userList.innerHTML = "";
+//   users.forEach((user) => {
+//     const li = document.createElement("div");
+//     li.setAttribute("class", "user");
+//     li.innerText = user.username;
+//     userList.appendChild(li);
+//   });
+// };
+
+const showJoinedUsersInfo = (usersArray) => {
+  const lis = document.querySelectorAll(".user");
+  console.log("lis.legnth: ", lis.length);
+  console.log("usersArray.legnth: ", usersArray.length);
+
+  if (usersArray.length !== 0) {
+    //   clear user list
+    for (let i = 0; i < lis.length; i++) {
+      lis[i].remove();
+    }
+  }
+
+  //   add user list to DOM
+  usersArray.map((user) => {
+    //   console.log(`user: ${JSON.stringify(user.username)}`);
+    const li = document.createElement("div");
+    li.setAttribute("class", "user");
+    li.setAttribute("id", user.id);
+    li.innerText = user.username;
+    userList.appendChild(li);
+  });
+};
 
 const addMessageToDom = (msg, testUserName) => {
   const div = document.createElement("div");
@@ -23,19 +70,40 @@ const addMessageToDom = (msg, testUserName) => {
   userName.innerText = testUserName;
   message.innerText = `15:35`;
   div.appendChild(userName);
+
   div.appendChild(time);
   div.appendChild(message);
   messages.appendChild(div);
 };
 
-socket.on("chat message", function (msg, testUserName) {
+const showRoomInfo = (roomName) => {
+  headerRoomName.innerText = roomName;
+};
+
+socket.on("message", function (
+  msg,
+  testUserName,
+  roomName = "",
+  joinedUsersArray = []
+) {
   console.log(`msg: ${msg}`);
+  console.log(`roomName: ${roomName}`);
   console.log(`testUserName: ${testUserName}`);
+  console.log(`joinedUsersArray: ${JSON.stringify(joinedUsersArray)}`);
+  console.log(`joinedUsersArray.length: ${joinedUsersArray.length}`);
   //   Output message to DOM
   // $("#messages").append($("<li>").text(msg));
   addMessageToDom(msg, testUserName);
+
+  //   방정보 보여주기
+  //   이건 한 번만 보여주면 되지 않나?
+  //   여기에 있으면 매번 메시지 이벤트가 호출 될 때마다 시행되니까 안 좋은 듯
+  showRoomInfo(room);
+  if (joinedUsersArray.length != 0) {
+    // 대화에 참가하는 유저 보여주기
+    showJoinedUsersInfo(joinedUsersArray);
+  }
 });
-// });
 
 chatForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -57,4 +125,47 @@ chatForm.addEventListener("submit", (e) => {
   // Clear input
   e.target.elements.msg.value = "";
   e.target.elements.msg.focus();
+});
+
+// **************************************************
+// “{user} is typing” functionality
+// **************************************************
+const showWhoIsTyping = (testUserName) => {
+  //   console.log(`show typing `);
+  typingSpan.innerText = testUserName + keyDownOne;
+};
+
+const clearTypingInfo = () => {
+  //   console.log(`clear typing `);
+  typingSpan.innerText = "";
+};
+
+socket.on("typing", (testUserName) => {
+  showWhoIsTyping(testUserName);
+});
+socket.on("doneTyping", (testUserName) => {
+  clearTypingInfo();
+});
+
+chatForm.addEventListener("keydown", (e) => {
+  //   console.log("keydown" + e);
+  //   console.log(JSON.stringify(e));
+
+  socket.emit("keyDown");
+});
+
+chatForm.addEventListener("keyup", (e) => {
+  socket.emit("keyUp");
+
+  clearTypingInfo();
+});
+
+// **************************************************
+// click events
+// **************************************************
+userList.addEventListener("click", (e) => {
+  // private chatting starts
+  if (e.target.classList.contains("user")) {
+    // console.log(e.target.id);
+  }
 });
